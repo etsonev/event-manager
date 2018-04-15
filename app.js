19e272b70a6f72f1182edc011e127e9704dcb933
etsonev <emiltsonev96@abv.bg>
2018-04-15 16:23:31 +0300

    Added setup for express-handlebars and added the index view

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 
 const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Index Route
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
+});
+
+
 
 const port = 5000;
 
